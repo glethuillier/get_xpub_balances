8b6b9cf93a083da2e37269182ccf5b2fd6404476
Guillaume Lethuillier <guillaume.lethuillier@ledger.fr>
2021-02-12 11:35:28 +0100

    feat(bitcoin): modularize coins

--- settings.js
+++ settings.js
@@ -3,7 +3,10 @@ Object.freeze(AddressType);
 
 // blockstream API to retrieve stats related to a given address
 // (less rate limited than the blockchain.info one)
-const blockstreamAPI = 'https://blockstream.info/api/address/';
+const bitcoinAPI = 'https://blockstream.info/api/address/';
+
+// TODO(litecoin)
+const litecoinAPI = 'https://api.blockcypher.com/v1/ltc/main/addrs/'
 
 // max number of addresses to probe when checking a possible gap between derivation indices
 // (that is: range of indices not used for derivation)
@@ -14,4 +17,38 @@ const ADDRESSES_PREGENERATION = 2000;
 
 const VERBOSE = false;
 
-module.exports = { AddressType, blockstreamAPI, MAX_EXPLORATION, VERBOSE, ADDRESSES_PREGENERATION }
+
+const BITCOIN_NETWORK = {
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4,
+    },
+    pubKeyHash: 0x00,
+    scriptHash: 0x05,
+    wif: 0x80,
+  };
+
+// TODO(litecoin)
+const LITECOIN_NETWORK = {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bech32: 'ltc',
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe
+    },
+    pubKeyHash: 0x30,
+    scriptHash: 0x32,
+    wif: 0xb0
+  }
+
+module.exports = { 
+    AddressType, 
+    bitcoinAPI, 
+    MAX_EXPLORATION, 
+    VERBOSE, 
+    ADDRESSES_PREGENERATION,
+    BITCOIN_NETWORK,
+    LITECOIN_NETWORK
+}
